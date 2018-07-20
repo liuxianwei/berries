9e4285664e49a20d1c988f54e98f9907e7a9a433
虫虫爱蜗牛 <176223390@qq.com>
2018-07-20 14:54:38 +0800

    清空之前老旧的代码

--- berries-web/src/main/java/com/lee/berries/web/interceptor/BerriesInterceptor.java
+++
@@ -1,7 +0,0 @@
-package com.lee.berries.web.interceptor;
-
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-public class BerriesInterceptor extends HandlerInterceptorAdapter{
-
-}
