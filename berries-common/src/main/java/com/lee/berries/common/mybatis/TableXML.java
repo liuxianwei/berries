4b243466548b9b8deb584292bbae7268b102a33b
Liu xianwei <Liu xianwei@Liuxianwei>
2018-11-01 17:01:23 +0800

    修改jar发布位置

--- berries-common/src/main/java/com/lee/berries/common/mybatis/TableXML.java
+++ berries-common/src/main/java/com/lee/berries/common/mybatis/TableXML.java
@@ -5,10 +5,10 @@ import com.lee.berries.common.utils.BerriesUtils;
 public class TableXML {
 
 	public static void main(String []args) {
-		String tablesNames = "gm_activity, gm_ad, gm_bind_type, gm_brokerage_details, gm_brokerage_report, gm_category, gm_game, gm_game_category, gm_game_user, gm_gift, gm_gift_cd_key, gm_gift_record, gm_order, gm_play_record, gm_provider, gm_provider_order, gm_provider_user, gm_proxy, gm_proxy_user, gm_shared_link";
-		String tableXMLTemp = "<table tableName=\"@tableName\" domainObjectName=\"@poName\"\r\n" + 
+		String tablesNames = "base_area, base_construction_type, base_cost_highland, base_cost_highway_classification, base_cost_rainy, base_cost_sandstorm, base_cost_terrain, base_cost_traffic_interference, base_cost_type, base_engineering_stage, base_fee_item, base_fee_item_setting, base_fee_type, base_preparation, base_project_type, base_unit, sys_cost_template, sys_fee_extend, sys_labour, sys_labour_machine_fee_details, sys_machine_fee_item, sys_price_foundation, sys_price_standard, sys_quota_store, sys_quota_store_details, sys_quota_store_expend, usr_fee_template, usr_fee_template_details, usr_fee_template_setting, usr_labour, usr_my_labour, usr_project, usr_project_cost_details, usr_project_cost_expend, usr_project_fee, usr_project_info, usr_project_labour, usr_project_labour_machine_fee_details";
+		String tableXMLTemp = "        <table tableName=\"@tableName\" domainObjectName=\"@poName\"\r\n" + 
 				"        	enableInsert=\"false\" \r\n" + 
-				"       	enableSelectByExample=\"false\"\r\n" + 
+				"        	enableSelectByExample=\"false\"\r\n" + 
 				"        	enableUpdateByPrimaryKey=\"false\"\r\n" + 
 				"        	enableDeleteByPrimaryKey=\"false\"\r\n" + 
 				"        	enableDeleteByExample=\"false\"\r\n" + 
@@ -18,7 +18,11 @@ public class TableXML {
 	  String xml = "";	
 	  for(String t:tablesNames.split(",")) {
 		  t = t.trim();
-		  String itemXML = tableXMLTemp.replace("@tableName", t).replace("@poName", BerriesUtils.underlineToUpperCamelCase(t.replace("gm_", "")));
+		  String tableName = t;
+		  t = t.replace("base_", "");
+		  t = t.replace("usr_", "");
+		  t = t.replace("sys_", "");
+		  String itemXML = tableXMLTemp.replace("@tableName", tableName).replace("@poName", BerriesUtils.underlineToUpperCamelCase(t));
 		  xml += itemXML;
 	  }
 	  System.out.println(xml);
